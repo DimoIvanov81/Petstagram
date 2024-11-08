34f82b4da7bbfda656eea1f4d12f4a3211ae1795
DimoIvanov81 <dimo.ivanov.lex@gmail.com>
2024-11-08 11:30:01 +0200

    fix: fixing syntax error

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -3,11 +3,14 @@ from django.template.defaultfilters import slugify
 
 
 # Create your models here.
-class Pets(models.Model):
-    name = models.CharField(max_length=30),
-    personal_photo = models.URLField(),
-    date_of_birth = models.DateField(blank=True, null=True),
-    slug = models.SlugField(blank=True, null=False, unique=True),
+class Pet(models.Model):
+    name = models.CharField(max_length=30, default="Unnamed")
+
+    personal_photo = models.URLField(blank=True, null=True)
+
+    date_of_birth = models.DateField(blank=True, null=True)
+
+    slug = models.SlugField(blank=True, null=False, unique=True)
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
